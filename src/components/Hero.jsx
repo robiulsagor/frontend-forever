b780afc4868b7c7be08c5a68dd9e59576e92808a
robiulsagor <robiul100.me@gmail.com>
2024-09-19 09:23:27 +0600

    some style change in hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,17 +6,19 @@ const Hero = () => {
         {/* hero left side */}
         <div className="w-full sm:w-1/2 flex items-center justify-center py-10 sm:py-0">
             <div className="text-[#414141]">
-                <div className="flex gap-3 items-center">
-                    <p className="w-8 md:w-11 h-[1.5px] bg-[#414141]"/>
+                <div className="flex gap-2 items-center">
+                    <p className="w-8 md:w-11 h-[1.6px] bg-[#414141]"/>
                     <p className="font-medium text-sm md:text-base uppercase">Our Bestsellers</p>
                 </div>
                 <h1 className="text-2xl sm:text-3xl lg:text-5xl py-3 prata">Latest Arrivals</h1>
-                <div className="flex gap-3 items-center pt-2">
-                <p className="uppercase text-sm md:text-base text-gray-800 font-medium">Shop Now</p>
-                <p className="w-8 md:w-11 h-[1.5px] bg-[#414141]"/>
+                <div className="flex gap-2 items-center ">
+                <p className="uppercase text-sm md:text-base text-gray-800 font-semibold">Shop Now</p>
+                <p className="w-8 md:w-11 h-[1.6px] bg-[#414141]"/>
                 </div>
             </div>
         </div>
+
+
 
         {/* hero right side */}
         <img src={assets.hero_img} className=" w-full sm:w-1/2" alt="" />
